722b28f8db80549e4a1a92cc89ed79b33b3fb99c
Frantz FISCHER <ffischer@nuxeo.com>
2017-03-31 17:58:17 +0200

    After review changes

--- upgrade_tomcat7.sh
+++ upgrade_tomcat7.sh
@@ -6,22 +6,25 @@ TOMCAT_ARCHIVE_URL="https://archive.apache.org/dist/tomcat/tomcat-7"
 TOMCAT_LATEST_URL="http://www.apache.org/dist/tomcat/tomcat-7"
 
 usage() {
-  echo -ne "Usage:\n\t./upgrade_tomcat7.sh NUXEO_HOME [TOMCAT_TARGET_VERSION] [TEMPORARY_WORK_FOLDER]\n\n"
-  echo -ne "Note: If no target version is specified the latest one will be retrieved from the Tomcat site\n\n"
-  echo -ne "Examples:\n"
-  echo -ne "\t./upgrade_tomcat7.sh /Users/ffischer/nuxeo-cap-7.10-tomcat\n"
-  echo -ne "\t./upgrade_tomcat7.sh /Users/ffischer/nuxeo-cap-7.10-tomcat 7.0.76\n"
-  echo -ne "\t./upgrade_tomcat7.sh /Users/ffischer/nuxeo-cap-7.10-tomcat 7.0.76 /tmp\n"
+  echo "Usage:"
+  echo -e "\t./upgrade_tomcat7.sh NUXEO_HOME [TOMCAT_TARGET_VERSION]"
+  echo
+  echo "Note: If no target version is specified the latest one will be retrieved from the Tomcat site"
+  echo
+  echo "Examples:"
+  echo -e "\t./upgrade_tomcat7.sh /Users/ffischer/nuxeo-cap-7.10-tomcat"
+  echo -e "\t./upgrade_tomcat7.sh /Users/ffischer/nuxeo-cap-7.10-tomcat 7.0.76"
 }
 
-if [ $# -eq 0 ] || [ $# -gt 3 ]; then
+if [ $# -eq 0 -o $# -gt 3 ]; then
   usage
   exit 1
 fi
 
 # does the Nuxeo location seem valid?
 if [ ! -f "$1/templates/nuxeo.defaults" ]; then
-  echo -ne "ERROR: Cannot find nuxeo.defaults file. Please check the Nuxeo location.\n\n"
+  echo "ERROR: Cannot find nuxeo.defaults file. Please check the Nuxeo location."
+  echo
   exit 1
 fi
 NUXEO_HOME=$1
@@ -41,15 +44,14 @@ else
   fi
 fi
 
-WORK_FOLDER=${3-/tmp}
-if [ ! -d "$WORK_FOLDER" ]; then
-  echo -ne "ERROR: Temporary work folder does not exists.\n\n"
+WORK_FOLDER=`mktemp -d -q /tmp/$(basename $0).XXXXXX`
+if [ $? -ne 0 ]; then
+  echo "ERROR: Cannot create temp file, exiting..."
   exit 1
 fi
-DOWNLOAD_FOLDER=${WORK_FOLDER}/nuxeo_downloads
 
-TOMCAT_SOURCE=$(java -cp ${NUXEO_HOME}/lib/catalina.jar org.apache.catalina.util.ServerInfo | grep "Server number" | sed 's/.*\(7\.0.[0-9]*\).*/\1/g')
-TOMCAT_NUXEO_DEFAULT=$(cat ${NUXEO_HOME}/templates/nuxeo.defaults | grep tomcat.version | cut -d "=" -f 2)
+TOMCAT_SOURCE=$(java -cp "${NUXEO_HOME}/lib/catalina.jar" org.apache.catalina.util.ServerInfo | grep "Server number" | sed 's/.*\(7\.0.[0-9]*\).*/\1/g')
+TOMCAT_NUXEO_DEFAULT=$(cat "${NUXEO_HOME}/templates/nuxeo.defaults" | grep tomcat.version | cut -d "=" -f 2)
 
 echo "NUXEO_HOME is ${NUXEO_HOME}"
 echo "TEMPORARY WORK FOLDER is ${WORK_FOLDER}"
@@ -61,12 +63,13 @@ else
   echo "ERROR: TOMCAT source versions don't match!"
   exit 1
 fi
-echo -ne "TOMCAT target version is ${TOMCAT_TARGET}\n\n"
+echo "TOMCAT target version is ${TOMCAT_TARGET}"
+echo
 
 echo "Retrieving files..."
-rm -rf ${DOWNLOAD_FOLDER}
-mkdir -p ${DOWNLOAD_FOLDER}
-cd ${DOWNLOAD_FOLDER}
+rm -rf "${WORK_FOLDER}"
+mkdir -p "${WORK_FOLDER}"
+cd "${WORK_FOLDER}"
 wget ${TOMCAT_ARCHIVE_URL}/v${TOMCAT_TARGET}/bin/apache-tomcat-${TOMCAT_TARGET}.tar.gz || exit 1
 wget ${TOMCAT_ARCHIVE_URL}/v${TOMCAT_TARGET}/bin/apache-tomcat-${TOMCAT_TARGET}.tar.gz.md5 || exit 1
 wget ${TOMCAT_ARCHIVE_URL}/v${TOMCAT_TARGET}/bin/apache-tomcat-${TOMCAT_TARGET}.tar.gz.sha1 || exit 1
@@ -86,30 +89,30 @@ sed -i 's/\*/ /g' tomcat-juli-adapters.jar.sha1; echo >> tomcat-juli-adapters.ja
 sed -i 's/\*/ /g' tomcat-juli.jar.md5; echo >> tomcat-juli.jar.md5
 sed -i 's/\*/ /g' tomcat-juli.jar.sha1; echo >> tomcat-juli.jar.sha1
 # perform checks
-md5sum -c apache-tomcat-${TOMCAT_TARGET}.tar.gz.md5
-shasum -c apache-tomcat-${TOMCAT_TARGET}.tar.gz.sha1
-md5sum -c tomcat-juli-adapters.jar.md5
-shasum -c tomcat-juli-adapters.jar.sha1
-md5sum -c tomcat-juli.jar.md5
-shasum -c tomcat-juli.jar.sha1
+md5sum -c apache-tomcat-${TOMCAT_TARGET}.tar.gz.md5 || exit 1
+shasum -c apache-tomcat-${TOMCAT_TARGET}.tar.gz.sha1 || exit 1
+md5sum -c tomcat-juli-adapters.jar.md5 || exit 1
+shasum -c tomcat-juli-adapters.jar.sha1 || exit 1
+md5sum -c tomcat-juli.jar.md5 || exit 1
+shasum -c tomcat-juli.jar.sha1 || exit 1
 
 echo "Patching Nuxeo..."
 # upgrading files from core distribution
 tar zxf apache-tomcat-${TOMCAT_TARGET}.tar.gz
-cp apache-tomcat-${TOMCAT_TARGET}/lib/* ${NUXEO_HOME}/lib
-cp apache-tomcat-${TOMCAT_TARGET}/bin/*.jar ${NUXEO_HOME}/bin
-cp apache-tomcat-${TOMCAT_TARGET}/bin/catalina-tasks.xml ${NUXEO_HOME}/bin
-cp apache-tomcat-${TOMCAT_TARGET}/lib/tomcat-jdbc.jar ${NUXEO_HOME}/nxserver/lib/tomcat-jdbc-${TOMCAT_TARGET}.jar
-rm ${NUXEO_HOME}/nxserver/lib/tomcat-jdbc-${TOMCAT_SOURCE}.jar
-cp apache-tomcat-${TOMCAT_TARGET}/bin/tomcat-juli.jar ${NUXEO_HOME}/nxserver/lib/tomcat-juli-${TOMCAT_TARGET}.jar
-rm ${NUXEO_HOME}/nxserver/lib/tomcat-juli-${TOMCAT_SOURCE}.jar
+cp apache-tomcat-${TOMCAT_TARGET}/lib/* "${NUXEO_HOME}/lib"
+cp apache-tomcat-${TOMCAT_TARGET}/bin/*.jar "${NUXEO_HOME}/bin"
+cp apache-tomcat-${TOMCAT_TARGET}/bin/catalina-tasks.xml "${NUXEO_HOME}/bin"
+cp apache-tomcat-${TOMCAT_TARGET}/lib/tomcat-jdbc.jar "${NUXEO_HOME}/nxserver/lib/tomcat-jdbc-${TOMCAT_TARGET}.jar"
+rm "${NUXEO_HOME}/nxserver/lib/tomcat-jdbc-${TOMCAT_SOURCE}.jar"
+cp apache-tomcat-${TOMCAT_TARGET}/bin/tomcat-juli.jar "${NUXEO_HOME}/nxserver/lib/tomcat-juli-${TOMCAT_TARGET}.jar"
+rm "${NUXEO_HOME}/nxserver/lib/tomcat-juli-${TOMCAT_SOURCE}.jar"
 # upgrading files from extras
-cp tomcat-juli.jar ${NUXEO_HOME}/bin
-cp tomcat-juli-adapters.jar ${NUXEO_HOME}/lib
+cp tomcat-juli.jar "${NUXEO_HOME}/bin"
+cp tomcat-juli-adapters.jar "${NUXEO_HOME}/lib"
 # release files
-cp apache-tomcat-${TOMCAT_TARGET}/RELEASE-NOTES ${NUXEO_HOME}/doc-tomcat
-cp apache-tomcat-${TOMCAT_TARGET}/LICENSE ${NUXEO_HOME}/doc-tomcat
-cp apache-tomcat-${TOMCAT_TARGET}/NOTICE ${NUXEO_HOME}/doc-tomcat
-cp apache-tomcat-${TOMCAT_TARGET}/RUNNING.txt ${NUXEO_HOME}/doc-tomcat
+cp apache-tomcat-${TOMCAT_TARGET}/RELEASE-NOTES "${NUXEO_HOME}/doc-tomcat"
+cp apache-tomcat-${TOMCAT_TARGET}/LICENSE "${NUXEO_HOME}/doc-tomcat"
+cp apache-tomcat-${TOMCAT_TARGET}/NOTICE "${NUXEO_HOME}/doc-tomcat"
+cp apache-tomcat-${TOMCAT_TARGET}/RUNNING.txt "${NUXEO_HOME}/doc-tomcat"
 # nuxeo version bump
-sed -i 's/'$(echo "$TOMCAT_SOURCE" | sed 's/\./\\./g')'/'${TOMCAT_TARGET}'/g' ${NUXEO_HOME}/templates/nuxeo.defaults
+sed -i 's/'$(echo "$TOMCAT_SOURCE" | sed 's/\./\\./g')'/'${TOMCAT_TARGET}'/g' "${NUXEO_HOME}/templates/nuxeo.defaults"
